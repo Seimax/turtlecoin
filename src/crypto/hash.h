357b9256a21cd67105877adde0b54df37f8ef468
Brandon Lehmann <brandonlehmann@gmail.com>
2018-10-07 17:44:11 -0400

    Extended cn_slow_hash in support of upcoming changes
    Input: 0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02
    
    Cryptonight Fast Hash: b542df5b6e7f5f05275c98e7345884e2ac726aeeb07e03e44e0389eb86cd05f0
    Cryptonight v0: 1b606a3f4a07d6489a1bcd07697bd16696b61c8ae982f61a90160f4e52828a7f
    Cryptonight v1: c9fae8425d8688dc236bcdbc42fdb42d376c6ec190501aa84b04a4b4cf1ee122
    Cryptonight Lite v0: 28a22bad3f93d1408fca472eb5ad1cbe75f21d053c8ce5b3af105a57713e21dd
    Cryptonight Lite v1: 87c4e570653eb4c2b42b7a0d546559452dfab573b82ec52f152b7ff98e79446f

--- src/crypto/hash.h
+++ src/crypto/hash.h
@@ -12,6 +12,16 @@
 #include <CryptoTypes.h>
 #include "generic-ops.h"
 
+// CryptoNight Defintions Below
+
+// Standard Cryptonight Definitions
+#define CN_MEMORY                 2097152 // 2MB scratchpad 2^21
+#define CN_ITER                   1048576 // 2^20
+
+// Cryptonight Night Definitions
+#define CN_LITE_MEMORY            1048576 // 1MB scratchpad 2^20
+#define CN_LITE_ITER              524288 // 2^19
+
 namespace Crypto {
 
   extern "C" {
@@ -33,11 +43,11 @@ namespace Crypto {
   }
 
   inline void cn_slow_hash_v0(const void *data, size_t length, Hash &hash) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 0, 0, 0);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 0, 0, 0, CN_MEMORY, CN_ITER);
   }
 
   inline void cn_slow_hash_v1(const void *data, size_t length, Hash &hash) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 0, 1, 0);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 0, 1, 0, CN_MEMORY, CN_ITER);
   }
 
   inline void cn_slow_hash_v2(const void *data, size_t length, Hash &hash) {
@@ -45,11 +55,11 @@ namespace Crypto {
   }
 
   inline void cn_lite_slow_hash_v0(const void *data, size_t length, Hash &hash) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 0, 0);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 0, 0, CN_LITE_MEMORY, CN_LITE_ITER);
   }
 
   inline void cn_lite_slow_hash_v1(const void *data, size_t length, Hash &hash) {
-    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 1, 0);
+    cn_slow_hash(data, length, reinterpret_cast<char *>(&hash), 1, 1, 0, CN_LITE_MEMORY, CN_LITE_ITER);
   }
 
   inline void cn_lite_slow_hash_v2(const void *data, size_t length, Hash &hash) {
